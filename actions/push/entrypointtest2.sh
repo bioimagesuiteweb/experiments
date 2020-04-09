6237ab2f082809c48bf76e06cb38be4d46482488
Xenophon Papademetris <xenophon.papademetris@yale.edu>
2020-04-09 12:31:25 -0400

    update 4

--- actions/push/entrypointtest2.sh
+++ actions/push/entrypointtest2.sh
@@ -18,25 +18,29 @@ echo "----------------------------------------------------------"
 
 BASE=/basedir
 
+
 if [ -d  /hostfiles ]; then
+    # Running in persisent container, no need to build
     BASE=/hostfiles/biswebcontainer
+else
+    # New container configure everything
+    mkdir ${BASE}
+    echo "+++ Creating base directory inside the container in ${BASE}"
+    cd ${BASE}
+
+    export PATH=/usr/local/bin:${PATH}
+    cd ${BASE}
+    mkdir -p bisweb
+    cd bisweb
+    /usr/local/bin/biswebconfig.sh
 fi
-
-mkdir ${BASE}
-echo "+++ Creating base directory inside the container in ${BASE}"
-cd ${BASE}
-
-export PATH=/usr/local/bin:${PATH}
-cd ${BASE}
-mkdir -p bisweb
-cd bisweb
-/usr/local/bin/biswebconfig.sh
 
 # ------------------------------------------------------
 FIRST=10
 LAST=12
 BDIR=${BASE}/bisweb/src/build
-LOGFILE=${BDIR}/log.txt
+LOGFILE=${BDIR}/logjs.txt
+LOGFILE2=${BDIR}/logpy.txt
 RESULTFILE=${BDIR}/result.txt
 cd ${BDIR}
 
@@ -47,15 +51,14 @@ cd wasm;
 ctest -I ${FIRST},${LAST} -V | tee  -a ${LOGFILE}
 
 
-echo "----------------------------------------------------------"  | tee -a ${LOGFILE}
-echo "--- Regression testing Python" | tee -a ${LOGFILE}
-echo "---"  | tee -a ${LOGFILE}
+echo "----------------------------------------------------------"  | tee ${LOGFILE2}
+echo "--- Regression testing Python" | tee -a ${LOGFILE2}
+echo "---"  | tee -a ${LOGFILE2}
 cd ../native
 
-#ctest -I ${FIRST},${LAST} -V | tee  -a  ${LOGFILE}
+ctest -I ${FIRST},${LAST} -V | tee  -a  ${LOGFILE2}
 
-echo "----------------------------------------------------------"   | tee -a ${LOGFILE}
-
+echo "----------------------------------------------------------"   | tee -a ${LOGFILE2}
 
 cd ${BDIR}
 rm ${RESULTFILE}
@@ -64,7 +67,25 @@ echo "------------------------------------"
 echo "--- Postprocessing Result"
 echo "------------------------------------"
 
-grep "Test #" ${LOGFILE} > ${RESULTFILE}
+echo "...." > ${RESULTFILE}
+
+echo "...." >> ${RESULTFILE}
+echo ".... Javascript tests" >> ${RESULTFILE}
+echo "...." >> ${RESULTFILE}
+grep "Test #" ${LOGFILE} >> ${RESULTFILE}
+grep "passed" ${LOGFILE} | grep "failed" >> ${RESULTFILE}
+grep "Total Test time" ${LOGFILE} >> ${RESULTFILE}
+
+echo "...." >> ${RESULTFILE}
+echo "...." >> ${RESULTFILE}
+echo ".... Python tests" >> ${RESULTFILE}
+echo "...." >> ${RESULTFILE}
+grep "Test #" ${LOGFILE2} >> ${RESULTFILE}
+grep "passed" ${LOGFILE2} | grep "failed" >> ${RESULTFILE}
+grep "Total Test time" ${LOGFILE2} >> ${RESULTFILE}
+echo "...." >> ${RESULTFILE}
+
+
 echo "Reading results file"
 REPORT="$(cat ${RESULTFILE})"
 REPORT="${REPORT//'%'/'%25'}"
@@ -78,7 +99,7 @@ echo "--- Postprocessing Result Step2"
 echo "------------------------------------"
 
 echo "Reading full log file"
-LOG="$(cat ${LOGFILE} | tee)"
+LOG="$(cat ${LOGFILE}  ${LOGFILE2})"
 LOG="${LOG//'%'/'%25'}"
 LOG="${LOG//$'\n'/'%0A'}"
 LOG="${LOG//$'\r'/'%0D'}"
